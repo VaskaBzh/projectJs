773588770f736a448011636b5cf0faa1e50ff794
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-18 11:53:55 +0300

    finish second js project

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-let title = "проект";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 25000;
-let rollback = Math.round(Math.random() * 50);
-let fullPrice = 175000;
-let adaptive = true;
-let arr = screens.split('');
+let num = "266219";
+let mas = 1;
 
 
-console.log ('title'+ " " + title);
-console.log ('Стоимость разработки сайта' + " " + fullPrice + " " + 'рублей');
-console.log ('adaptive'+ " " + adaptive);
-console.log ('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей');
-console.log ('rollback'+ " " + rollback);
-console.log (screens.length);
-console.log (screens.toLowerCase());
-console.log (arr);
-console.log ('Процент отката посреднику за работу ' + " " + Math.round((fullPrice * (rollback/100))));
+num.toString().split('').forEach(elem => mas *= elem);
+console.log('mas: ', mas);
 
-
-console.log (typeof title);
-console.log (typeof fullPrice);
-console.log (typeof adaptive);
-console.log (typeof screenPrice);
-console.log (typeof rollback);
+let stn = mas ** 3;
+console.log(stn);
+console.log(stn.toString().substring(0,2));
