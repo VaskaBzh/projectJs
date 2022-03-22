eaa2adb30bf4fd57678b06f4bd81543a21f6c3ea
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-22 09:03:58 +0300

    react: rewright lesson4 js code

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ let title = prompt('Как называется ваш проект?', 'прое
 let screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
+
 let rollback = Math.round(Math.random() * 50);
+let fullPrice;
+let allServicePrices;
+let 
+
 let service1 = prompt('Какой дополнительный тип услуги нужен?', 'метрика');
 let servicePrice1 = +prompt('Сколько это будет стоить?', '500');
 let service2 = prompt('Какой дополнительный тип услуги нужен?', 'адаптация');
@@ -14,16 +19,16 @@ const getAllServicePrices = function (servicePrice1, servicePrice2) {
     return servicePrice1 + servicePrice2
 }
 
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+const showTypeOff = function(variable) {
+    console.log(variable, typeof variable);
+}
 
 const getFullPrice = function (screenPrice, allServicePrices) {
     return screenPrice + allServicePrices   
 }
 
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
-
-const showTypeOff = function(variable) {
-    console.log(variable, typeof variable);
+const getServicePercentPrices = function (fullPrice, rollback) {
+    return fullPrice - (fullPrice * (rollback/100))
 }
 
 const getRollbackMessage = function(fullPrice) {
@@ -38,22 +43,15 @@ const getRollbackMessage = function(fullPrice) {
     };
 }
 
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+
 const getTitle = function(title) {
-    let titleWords
-    if (! title.split('')[0] == ' ') {
-        titleWords = title.split('')[0].toUpperCase();
-    } else {
-        titleWords = title.split('')[1].toUpperCase();
-    };
-    let titleUpperCase = titleWords + title.slice(1);
-    return titleUpperCase
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
 }
 
 title = getTitle(title);
-
-const getServicePercentPrices = function (fullPrice, rollback) {
-    return fullPrice - (fullPrice * (rollback/100))
-}
 
 let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
