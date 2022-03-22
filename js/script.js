5c68bbc9a361b41a0f83b0ea5e6044889623976f
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-22 09:07:43 +0300

    react: rewright lesson4 js code

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let adaptive = confirm('Нужен ли адаптив на сайте?');
 let rollback = Math.round(Math.random() * 50);
 let fullPrice;
 let allServicePrices;
-let 
+let servicePercentPrice;
 
 let service1 = prompt('Какой дополнительный тип услуги нужен?', 'метрика');
 let servicePrice1 = +prompt('Сколько это будет стоить?', '500');
@@ -43,18 +43,14 @@ const getRollbackMessage = function(fullPrice) {
     };
 }
 
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
-
 const getTitle = function(title) {
     return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
 }
 
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 title = getTitle(title);
-
-let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-
 
 showTypeOff(title)
 showTypeOff(fullPrice)
