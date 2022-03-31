70884630d67d3fd9e10c93a5f26a77d9254413e7
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-31 11:55:40 +0300

    feat: finish lesson8

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [  ],
     screenPrice: 0,
     adaptive: true,
     rollback: Math.round(Math.random() * 100),
     fullPrice: 0,
     allServicePrices: 0,
     servicePercentPrices: 0,
-    service1: '',
-    service2: '',
+    services: {},
     getNumber: function (num) {
         let numNew = Number(String(num).trim())
         return numNew
@@ -19,45 +18,59 @@ const appData = {
         return !isNaN(parseFloat(num)) && isFinite(num) && num != 0
     },
     asking: function() {
-        appData.title = prompt('Как называется ваш проект?', 'проект')
-        appData.screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные")
-        
         do {
-            appData.screenPrice = +prompt('Сколько будет стоить данная работа?', '12000')
-            appData.getNumber(appData.screenPrice)
-        } while (!appData.isNumber(appData.screenPrice))
+            appData.title = prompt('Как называется ваш проект?', "Проект");
+        } while (isNaN(appData.title) === false);
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0
+            
+            do {
+                name = prompt('Какие типы экранов нужно разработать?', 'Простые');
+            } while (isNaN(name) === false);
+            
+            do {
+                price = +prompt('Сколько будет стоить данная работа?', '12000')
+                appData.getNumber(price)
+            } while (!appData.isNumber(price))
+
+            appData.screens.push({ id: 1, name: name, price: price });
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0
+            
+            do {
+                name = prompt('Какой дополнительный тип услуги нужен?', 'метрика');
+            } while (isNaN(name) === false);
+    
+            do {
+                price = +prompt("Сколько это будет стоить?", '500')
+                appData.getNumber(price)
+            } while (!appData.isNumber(price))
+
+            appData.services[i + 1 + " " + name] = +price
+        }
             
         appData.adaptive = confirm('Нужен ли адаптив на сайте?')
     },
-    getAllServicePrices: function () {
-        let sum = 0
-        let sumControl
-
-        for (let i = 0; i < 2; i++) {
-
-        if (i === 0) {
-            appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'метрика')
-        } else if (i === 1) {
-            appData.service2 = prompt('Какой дополнительный тип услуги нужен?', 'адаптация')
+    addPrices: function() {
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.price
         }
-
-        do {
-            sumControl = +prompt("Сколько это будет стоить?", '500')
-            appData.getNumber(sumControl)
-        } while (!appData.isNumber(sumControl))
-            sum += sumControl
+        
+        for (let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
         }
-
-        return sum
     },
     showTypeOff: function(variable) {
         console.log(variable, typeof variable);
     },
     getFullPrice: function () {
-        return appData.screenPrice + appData.allServicePrices   
+        appData.fullPrice = appData.screenPrice + appData.allServicePrices   
     },
     getServicePercentPrices: function () {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+        appData.servicePercentPrices = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
     },
     getRollbackMessage: function() {
         if (appData.fullPrice >= 30000){
@@ -71,7 +84,7 @@ const appData = {
         }
     },
     getTitle: function() {
-        return appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase()
+        appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase()
     },
     logger: function () {
         for(let prop in appData) {
@@ -83,10 +96,10 @@ const appData = {
     start: function () {
         appData.getNumber()
         appData.asking()
-        appData.allServicePrices = appData.getAllServicePrices()
-        appData.fullPrice = appData.getFullPrice()
-        appData.servicePercentPrices = appData.getServicePercentPrices()
-        appData.title = appData.getTitle()
+        appData.addPrices()
+        appData.getFullPrice()
+        appData.getServicePercentPrices()
+        appData.getTitle()
         
         appData.logger()
     }
